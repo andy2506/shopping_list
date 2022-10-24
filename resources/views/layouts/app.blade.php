41cea28eec11b7a0822868b8a802d9e7b8245d9d
Andile <Andile.Kweyama@bwscloud.tech>
2022-10-24 06:42:32 +0200

    Updated style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,9 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script> -->
 
     <!-- Styles -->
     <style>
@@ -67,8 +67,16 @@
         .m-b-md {
             margin-bottom: 30px;
         }
-        .container-wrapper{width: 70%; margin: 0 auto;}
-        .bold{font-weight: bold;}
+        .container-wrapper{
+            width: 70%; 
+            margin: 0 auto;
+        }
+        .bold{
+            font-weight: bold;
+        }
+        h3.heading {
+            color: #fff;
+        }
     </style>
 </head>
 <body>
